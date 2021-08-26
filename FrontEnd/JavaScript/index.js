42d2a8259b97a9b6e8abc77bd4a6e521ce145c7a
Tristan Collins <tpcollins@crimson.ua.edu>
2021-08-25 23:50:27 -0500

    pointed API

--- FrontEnd/JavaScript/index.js
+++ FrontEnd/JavaScript/index.js
@@ -4,9 +4,15 @@ function handleShowDate(){
     fetch(apiUrl).then(function(response){
         return response.json();
     }).then(function(json){
+        console.log(json);
         console.log(json)
         var dateUrl = json[0].year;
-        var html = "<div>" + dateUrl + "</div>";
+        var html = "<div id=\"date\">" + dateUrl;
+        json.forEach(dateUrl => {
+            html += " ";
+            html += dateUrl.year;
+        });
+        html += "</div>";
         document.getElementById("date").innerHTML = html;
     }).catch(function(error){
         console.log(error);
